ff4f2ce910222cb047d004bf8a48eec435e993c0
= <mytec@me.com>
2025-04-26 14:24:42 -0400

    Removed redundant test

--- Wordlebot.Tests/Wordlebot_Game.cs
+++ Wordlebot.Tests/Wordlebot_Game.cs
@@ -35,34 +35,6 @@ namespace Wordlebot.Tests
         }
 
         [Theory]
-        [ClassData(typeof(GameResults_Arose))]
-        public void GetWordleResult_Arose(string startingWord, string wordle, string expected)
-        {
-            string? projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.Parent?.FullName;
-            if (projectDirectory == null)
-            {
-                Assert.Fail($"Wordlist file not found.");
-            }
-
-            var logger = new ConsoleLogger("quiet");
-            string WordListFile = Path.Combine(projectDirectory, "Wordlebot", "5_letter_words_official.txt");
-            var wordlist = new WordleWordList(WordListFile, logger);
-
-            var args = new ProgramArguments
-            {
-                ResultOnly = true,
-                StartingWord = startingWord,
-                Wordle = wordle,
-                WordListFile = ""
-            };
-
-            var game = new WordleGame(logger, wordlist, args);
-            var result = game.PlayWordle();
-
-            Assert.Equal(expected, result);
-        }
-
-        [Theory]
         [ClassData(typeof(VersusWordleBot))]
         public void VersusWordleBot(string startingWord, string wordle, string expected, string wordleResult)
         {
